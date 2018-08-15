d6f8a2f76291d60cdce87fe56baef10415b94598
Soren's Coding Land <chaz.com@hotmail.co.uk>
2018-08-15 17:34:25 +0100

    Lecture 123 - [ExecuteInEditMode] - Done.
    Most of this lecture was spent struggling to understand Ben's explanation ofthe snap to grid script that we made. Throughout I was confused.
    
    I learned that I don't remember much about Range arrays and that Unity docs for it almost don't exsist.
    
    I learned that we can run scripts in the editor, which are useful for controlling what the designer can and can't do. We use this to control the snapping to grid ofhte cubes.

---
+++ Realm Rush/Assets/EditorSnap.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class EditorSnap : MonoBehaviour {
+
+    [SerializeField] [Range (1f, 20f)] float gridSize = 10f; // Range restricts the values that can be set in the inspector to between 1 and 20 float.
+                                                            // Why 20? Incase you need space for cubes smaller than 10f.
+
+    void Update()
+        {
+        Vector3 snapPos;
+        snapPos.x = Mathf.RoundToInt(transform.position.x / gridSize) * gridSize; // Rounds up to a whole Int number in x divide by 10 float. Then multiply by 10.
+                                                                        // How? The 10 is the size of the block in x axis. And divided by 10 makes .625 or whatever.
+                                                                        // Then it's rounded up to the whole number and then multiplied to 6 in order to be big enough again.
+        snapPos.z = Mathf.RoundToInt(transform.position.z / gridSize) * gridSize; // Need to work this out like the above x position.
+
+        transform.position = new Vector3(snapPos.x, 0f, snapPos.z);
+        }
+}
