d4a191295640471fa000dd48d45223cbfda0d7a4
Soren's Coding Land <chaz.com@hotmail.co.uk>
2018-08-15 20:31:50 +0100

    Lecture 124 - Using Text Mesh For Labels - Done.
    We learned about text meshs, font size, character size and resolution.
    Also an interesting bit of code that I don't remember what it is but I remember what it does and that it's near the top of the script.
    It makes it so that you click on the parent object in the scene window rather than the child objects. This stops the designer breaking things.

--- Realm Rush/Assets/CubeEditor.cs
+++ Realm Rush/Assets/CubeEditor.cs
@@ -3,19 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [ExecuteInEditMode]
-public class EditorSnap : MonoBehaviour {
+[SelectionBase] // Makes it so you end selecting the parent object rather than the child object.
+public class CubeEditor : MonoBehaviour {
 
     [SerializeField] [Range (1f, 20f)] float gridSize = 10f; // Range restricts the values that can be set in the inspector to between 1 and 20 float.
-                                                            // Why 20? Incase you need space for cubes smaller than 10f.
+                                                             // Why 20? Incase you need space for cubes smaller than 10f.
+
+    TextMesh textMesh;
 
     void Update()
         {
         Vector3 snapPos;
+
         snapPos.x = Mathf.RoundToInt(transform.position.x / gridSize) * gridSize; // Rounds up to a whole Int number in x divide by 10 float. Then multiply by 10.
                                                                         // How? The 10 is the size of the block in x axis. And divided by 10 makes .625 or whatever.
                                                                         // Then it's rounded up to the whole number and then multiplied to 6 in order to be big enough again.
         snapPos.z = Mathf.RoundToInt(transform.position.z / gridSize) * gridSize; // Need to work this out like the above x position.
 
         transform.position = new Vector3(snapPos.x, 0f, snapPos.z);
+
+        textMesh = GetComponentInChildren<TextMesh>();
+        textMesh.text = snapPos.x / gridSize + "," + snapPos.z / gridSize; // It divides by grid size here so that you end up with whole numbers in the text.
         }
 }
