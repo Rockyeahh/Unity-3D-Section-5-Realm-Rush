b0447920ecf1d2f586d2f4d9f7db7e58519c3168
Soren's Coding Land <chaz.com@hotmail.co.uk>
2018-10-15 21:04:25 +0100

    Lecture 141 - Another Solo Challenge - Half way through.
    Nellie has been a pain all day and I haven't completedthis single challenge today. FIFBVHFYCVBJN THIS SHIT. I'll try again tommroow and hopefully get ...... I'm unable to use my brain enough to finish that sentence.
    
    Here's something I had copied and ready for pasting.
    I will have to stop now as I've spent so much time away from this due to Nellie that I'm running out of time to get work done. I'll do more tomorrow before work. Also I'm very angry and that clouds my judgement.

---
+++ Realm Rush/Assets/Scripts/EnemyHealth.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour {
+
+    // Enemyhealth
+    [SerializeField] int enemyHealth = 10;
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+        // print Enemyhealth.
+        print(enemyHealth);
+	}
+
+    // particle collision method.
+    void OnParticleCollision(GameObject other)
+    {
+        print("Colliding");
+        ProcessHits();
+        if (enemyHealth < 1)
+        {
+            EnemyDies();
+        }
+    }
+
+    private void ProcessHits()
+    {
+        enemyHealth--;
+    }
+
+    // Death method.
+    private void EnemyDies()
+    {
+        Destroy(gameObject);
+    }
+
+}
