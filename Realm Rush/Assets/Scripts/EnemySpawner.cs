45562fa96e8912c89ea84a9e5b901b023f44f8b3
Soren's Coding Land <chaz.com@hotmail.co.uk>
2018-11-07 22:40:53 +0000

    Lecture 146 - Revise Coroutines & Much More - Started
    This one is supposed to be for us to back and study up on coroutines and then somehow make use of them but I'm too tired mentally to get anywhere withthis so sod it. I'll carry on tommorrow.

---
+++ Realm Rush/Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour {
+
+    [SerializeField] float secondsBetweenSpawns = 2f;
+    [SerializeField] GameObject enemyToSpawn;
+
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(SpawnEnemies(secondsBetweenSpawns));
+	}
+
+    IEnumerator SpawnEnemies() {if (secondsBetweenSpawns <= 0)
+        {
+            yield return new WaitForSeconds(1);
+            //or just Instantiate (enemyToSpawn, Vector3 stuff, Quaternion.identity);
+            Instantiate(enemyToSpawn, new Vector3(0, 0, 0), Quaternion.identity); // May need a different transform.
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
