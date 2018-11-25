023c75d13d077055d07041de3d2e12e1e4f155ea
Soren's Coding Land <chaz.com@hotmail.co.uk>
2018-11-25 16:30:10 +0000

    lecture 156 - Complete Game Loop - Done.
    The Enemies explode when they reach the base.

--- Realm Rush/Assets/Scripts/EnemyMovement.cs
+++ Realm Rush/Assets/Scripts/EnemyMovement.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 
 public class EnemyMovement : MonoBehaviour {
 
+    [SerializeField] float movementPeriod = .5f;
+    [SerializeField] ParticleSystem goalParticle;
+
     void Start () {
         Pathfinder pathfinder = FindObjectOfType<Pathfinder>(); // Only works as long as we have one Pathfinder script in the scene.
         var path = pathfinder.GetPath(); // Change var to List of waypoints.
@@ -13,12 +16,21 @@ public class EnemyMovement : MonoBehaviour {
 
     IEnumerator FollowPath(List<Waypoint> path) // I think you can access lists from any script.
     {
-        print("Starting Patrol");
         foreach (Waypoint waypoint in path)
         {
             transform.position = waypoint.transform.position;
-            yield return new WaitForSeconds(2f); // moves the enemy each second.
+            yield return new WaitForSeconds(movementPeriod); // moves the enemy each second.
         }
-        print("Ending Patrol");
+        SelfDestruct();
     }
+
+    private void SelfDestruct()
+    {
+        var vfx = Instantiate(goalParticle, transform.position, Quaternion.identity);
+        vfx.Play();
+        Destroy(vfx.gameObject, vfx.main.duration);
+
+        Destroy(gameObject); // destroy enemy
+    }
+
 }
