1e2ac3e229bdc4826cb8cbee8a6d33110a55cee8
Soren's Coding Land <chaz.com@hotmail.co.uk>
2018-11-29 13:44:46 +0000

    Lecture 157 - Health For Base - Done.
    The player base now has health and it decreases and prints "dies" when it reaches 0.

---
+++ Realm Rush/Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour {
+
+    [SerializeField] int playerHealth = 10;
+    [SerializeField] int playerHealthDecrease = 1;
+
+    void OnTriggerEnter(Collider collider)
+    {
+        playerHealth = playerHealth - playerHealthDecrease; // playerHealth is playerHealth - playerHealthDecrease.
+        //playerHealth--;
+        print(playerHealth);
+        if (playerHealth <= 0)
+        {
+            playerDies();
+        }
+    }
+
+    private void playerDies()
+    {
+        print("Player dies");
+    }
+}
